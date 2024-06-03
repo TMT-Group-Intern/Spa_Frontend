0569319d609e29a7a8f812a6a05a541e886d66bd
Khoi Hoang <lahoangkhoi.ck@gmail.com>
2024-06-03 11:07:21 +0700

    first commit

---
+++ spa_git/apps/salon/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TDS_I18N, vi_VN } from 'tds-ui/i18n';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+// Đa ngôn ngữ
+import localeVi from '@angular/common/locales/vi';
+import { registerLocaleData } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TDSButtonModule } from 'tds-ui/button';
+import { TDSButtonMenuModule } from 'tds-ui/button-menu';
+import { HomeComponent } from './home/home/home.component';
+registerLocaleData(localeVi);
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    ScrollingModule,
+    TDSButtonModule,
+    TDSButtonMenuModule,
+    HomeComponent
+  ],
+  providers: [{ provide: TDS_I18N, useValue: vi_VN }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
