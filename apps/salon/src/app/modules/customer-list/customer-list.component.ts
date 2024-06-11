d859816d89ce7cc03e298fa4bc299af69cf42bf2
Huynh Pham Tam Nguyen <tamnguyen.lisa@gmail.com>
2024-06-11 11:04:20 +0700

    Display Customer List through API

--- apps/salon/src/app/modules/customer-list/customer-list.component.ts
+++ apps/salon/src/app/modules/customer-list/customer-list.component.ts
@@ -4,28 +4,16 @@ import { TDSMapperPipeModule } from 'tds-ui/cdk/pipes/mapper';
 import { TDSDataTableModule } from 'tds-ui/data-table';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {  TDSTableQueryParams } from 'tds-ui/table';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { TDSColumnSettingsDTO, TDSColumnSettingsModule } from 'tds-ui/column-settings';
-import { Observable, catchError, of } from 'rxjs';
+import { TDSColumnSettingsModule } from 'tds-ui/column-settings';
 import { TDSListModule } from 'tds-ui/list';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TDSAvatarModule } from 'tds-ui/avatar';
 import { TDSModalModule } from 'tds-ui/modal';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { TDSFormFieldModule } from 'tds-ui/form-field';
 import { TDSSelectModule } from 'tds-ui/select';
-
-interface ItemData {
-  id: number;
-  name: string;
-  age: number;
-  address: string;
-  createDate: Date;
-  value: number;
-  avatar: string;
-}
+import { TDSRadioModule } from 'tds-ui/radio';
 
 @Component({
   selector: 'frontend-customer-list',
@@ -41,6 +29,7 @@ interface ItemData {
     TDSModalModule,
     TDSFormFieldModule,
     TDSSelectModule,
+    TDSRadioModule
   ],
   templateUrl: './customer-list.component.html',
   styleUrls: ['./customer-list.component.scss'],
@@ -48,91 +37,92 @@ interface ItemData {
 export class CustomerListComponent implements OnInit {
 
   isVisible = false;
+  radioValue = 'A';
+  signUpForm!: FormGroup;
+  CustomerList:any[] = [];
 
   constructor(
     private fb: FormBuilder,
     private auth : AuthService,
     private router:Router,
-) {
-    //this.guId = Guid.create();
-}
+  ) {}
 
+  // Display modal
   showModal(): void {
     this.isVisible = true;
   }
 
+
+  // Save button
   handleOk(): void {
     console.log("Button ok clicked!");
     this.isVisible = false;
   }
 
+  // Cancel button
   handleCancel(): void {
     console.log("Button cancel clicked!");
     this.isVisible = false;
   }
 
-  signUpForm!: FormGroup;
+//   persondisplayWith!: FormControl;
+//   public contactOptions = [
+//     { id: 1, name: 'Elton John' },
+//     { id: 2, name: 'Elvis Presley' },
+//     { id: 3, name: 'Paul McCartney' },
+//     { id: 4, name: 'Elton John' },
+//     { id: 5, name: 'Elvis Presley' },
+//     { id: 6, name: 'Paul McCartney' },
+// ``]
 
-  public contactOptions = [
-    { id: 1, name: 'Elton John' },
-    { id: 2, name: 'Elvis Presley' },
-    { id: 3, name: 'Paul McCartney' },
-    { id: 4, name: 'Elton John' },
-    { id: 5, name: 'Elvis Presley' },
-    { id: 6, name: 'Paul McCartney' },
-]
-
-persondisplayWith!: FormControl;
-
-  ngOnInit(): void {
-      this.signUpForm = this.fb.group({
-      // phoneNumber: ['', Validators.compose([
-      //     Validators.required,
-      //     Validators.pattern(/^[0-9]{10}$/i)
-      // ])],
-      email: [
-          '',
-          Validators.compose([
-          Validators.required,
-          Validators.email
-          ])
-      ],
-
-      taiKhoan:[
-        '' ,
-        Validators.required
-      ],
-
-      });
+  // Display Customer List
+  showCustomerList() {
+    this.auth.CustomerList().subscribe(data => {
+      this.CustomerList = data;
+      console.log('CustomerList:', this.CustomerList)
+    });
   }
 
-  listOfData: ItemData[] = [];
+  ngOnInit(): void {
 
-  //       ngOnInit(): void {
-  //           const data = [];
-  //           for (let i = 0; i < 50; i++) {
-  //               data.push({
-  //                   id: i,
-  //                   name: `Edward King ${i}`,
-  //                   age: 32,
-  //                   address: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-  //                   sed do eiusmod tempor incididunt ut
-  //                   labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-  //                   laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-  //                   voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-  //                   cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet,
-  //                   consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-  //                   labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-  //                   laboris nisi ut aliquip ex ea commodo consequat. ${i}`,
-  //                   createDate: new Date(),
-  //                   value: Math.floor(Math.random() * 200000000),
-  //                   avatar: `https://randomuser.me/api/portraits/women/${Math.floor(Math.random() * 100)}.jpg`,
-  //               });
-  //           }
-  //           this.listOfData = data;
-  // }
+    this.showCustomerList();
+
+    // this.signUpForm = this.fb.group({
+    //   // phoneNumber: ['', Validators.compose([
+    //   //     Validators.required,
+    //   //     Validators.pattern(/^[0-9]{10}$/i)
+    //   // ])],
+    //   email: [
+    //     '',
+    //     Validators.compose([
+    //     Validators.required,
+    //     Validators.email
+    //     ])
+    //   ],
+
+    //   taiKhoan:[
+    //     '' ,
+    //     Validators.required
+    //   ],
+
+    // });
+
+      // Display Customer List
+      // for (let i = 0; i < 50; i++) {
+      //   this.CustomerList.push({
+      //     Code: this.CustomerList.customerCode,
+      //     FirstName: this.CustomerList.firstName,
+      //     LastName: this.CustomerList.lastName,
+      //     Gender: this.CustomerList.gender,
+      //     PhoneNumber: this.CustomerList.phone,
+      //     Email: this.CustomerList.email,
+      //     DateOfBirth: this.CustomerList.dateOfBirth,
+      //     CustomerTypeID: this.CustomerList.customerTypeID,
+      //   });
+  }
 }
-function uuidv4() {
-  throw new Error('Function not implemented.');
-}
+
+// function uuidv4() {
+//   throw new Error('Function not implemented.');
+// }
 
