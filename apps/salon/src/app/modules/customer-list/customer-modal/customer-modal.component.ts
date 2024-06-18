b288e23bad12030654bd068f1c324546446f7fe9
nguyenvu2k2 <tuanvudzkt@gmail.com>
2024-06-18 08:48:51 +0700

    push 14/06/2024

--- apps/salon/src/app/modules/customer-list/customer-modal/customer-modal.component.ts
+++ apps/salon/src/app/modules/customer-list/customer-modal/customer-modal.component.ts
@@ -14,6 +14,7 @@ import { TDSDatePickerModule } from 'tds-ui/date-picker';
 import { CustomerListComponent } from '../customer-list.component';
 import { AuthService } from '../../../shared.service';
 import { catchError, of } from 'rxjs';
+import { error } from 'console';
 
 @Component({
   selector: 'frontend-customer-modal',
@@ -81,7 +82,12 @@ export class CustomerModalComponent implements OnInit {
 
   // Create Customer
   createCustomer(val: any) {
-    this.auth.CreateNewCustomer(val).subscribe(
+    this.auth.CreateNewCustomer(val).pipe(
+      catchError((ex) =>{
+        console.log(ex);
+        return of(null);
+      })
+    ).subscribe(
         {
           next: (v) => {
             this.modalService.success({
@@ -104,19 +110,21 @@ export class CustomerModalComponent implements OnInit {
   // Update Customer
   updateCustomer(id: number, val: any) {
     this.auth.UpdateCustomer(id, val).subscribe(
-      (res) => {
-        this.modalService.success({
-          title: 'Successfully!',
-          okText: 'OK',
-        });
-        this.modalRef.destroy(val);
-      },
-      (res) => {
-        this.modalService.error({
-          title: 'Fail!',
-          content: res.error.message,
-          okText: 'OK',
-        });
+      {
+        next: (res) => {
+          this.modalService.success({
+            title: 'Successfully!',
+            okText: 'OK',
+          });
+          this.modalRef.destroy(val);
+        },
+        error: (res) => {
+          this.modalService.error({
+            title: 'Fail!',
+            content: res.error.message,
+            okText: 'OK',
+          });
+        }
       }
     );
   }
