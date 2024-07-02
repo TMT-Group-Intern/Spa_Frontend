d7fb0e51ca459fa36159e3491528c170010904cb
Huynh Pham Tam Nguyen <tamnguyen.lisa@gmail.com>
2024-07-02 16:41:40 +0700

    Delete bug

--- apps/salon/src/app/shared.service.ts
+++ apps/salon/src/app/shared.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
-import { HttpHeaders } from '@angular/common/http'; 
+import { HttpHeaders } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 @Injectable({
@@ -10,59 +10,59 @@ import { throwError } from 'rxjs';
 })
 export class AuthService {
 
-  private baseUrl: string| undefined;
+  private baseUrl: string | undefined;
 
-  constructor(private http : HttpClient) {
-     this.baseUrl = environment.BASE_URI
+  constructor(private http: HttpClient) {
+    this.baseUrl = environment.BASE_URI
 
   }
 
   // get by id service
-  getByIdService(id:number): Observable<any[]>{
-    return this.http.get<any[]>(this.baseUrl + 'Services/'+ id);
+  getByIdService(id: number): Observable<any[]> {
+    return this.http.get<any[]>(this.baseUrl + 'Services/' + id);
   }
 
   // Render list of service
-  renderListService():Observable<any[]>{
+  renderListService(): Observable<any[]> {
     return this.http.get<any[]>(this.baseUrl + 'Services');
   }
 
   //Create a new service
-  createService(val:any){
+  createService(val: any) {
     return this.http.post(this.baseUrl + 'Services', val);
   }
 
   //Edit a service
-  editService(id: number,val:any){
-    return this.http.put(this.baseUrl + 'Services/'+ id, val);
+  editService(id: number, val: any) {
+    return this.http.put(this.baseUrl + 'Services/' + id, val);
   }
 
   //Delete a service
-  deleteAService(id:any){
-    return this.http.delete(this.baseUrl + 'Services/'+ id);
+  deleteAService(id: any) {
+    return this.http.delete(this.baseUrl + 'Services/' + id);
   }
-  UserList():Observable<any[]>{
-    return this.http.get<any>(this.baseUrl+ 'User/allUser2')
+  UserList(): Observable<any[]> {
+    return this.http.get<any>(this.baseUrl + 'User/allUser2')
   }
-  AdminList():Observable<any[]>{
-    return this.http.get<any>(this.baseUrl+ 'User/allAdmin')
+  AdminList(): Observable<any[]> {
+    return this.http.get<any>(this.baseUrl + 'User/allAdmin')
   }
-  EmpList():Observable<any[]>{
-    return this.http.get<any>(this.baseUrl+ 'User/allEmployee')
+  EmpList(): Observable<any[]> {
+    return this.http.get<any>(this.baseUrl + 'User/allEmployee')
   }
-  deleteUser(Email:string){
-    return this.http.delete(this.baseUrl + 'User/deleteUser?email='+ Email);
+  deleteUser(Email: string) {
+    return this.http.delete(this.baseUrl + 'User/deleteUser?email=' + Email);
   }
-  createUser(val:any){
+  createUser(val: any) {
     return this.http.post(this.baseUrl + 'Authentication/register', val);
   }
-  createAccountForEmployee(Email:string){
+  createAccountForEmployee(Email: string) {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     const body = { email: Email };
     return this.http.post(this.baseUrl + 'Authentication/CreateUserForEmployee', body, { headers });
   }
-  editUser(email: string,val:any){
-    return this.http.put(this.baseUrl + 'User/updateUser'+ email, val);
+  editUser(email: string, val: any) {
+    return this.http.put(this.baseUrl + 'User/updateUser' + email, val);
   }
   // Show list of Customer
   CustomerList(): Observable<any[]> {
@@ -75,33 +75,33 @@ export class AuthService {
   }
 
   // Search Customer
-  searchCustomer(val: any): Observable<{customers: any[]}> {
+  searchCustomer(val: any): Observable<{ customers: any[] }> {
     return this.http.get<any>(this.baseUrl + 'Customers/search?searchTerm=' + val);
   }
 
   // Create a new Customer
-  CreateNewCustomer(val:any) {
+  CreateNewCustomer(val: any) {
     return this.http.post(this.baseUrl + 'Customers', val);
   }
 
   // Update Customer
-  UpdateCustomer(id: number, val:any) {
+  UpdateCustomer(id: number, val: any) {
     return this.http.put(this.baseUrl + 'Customers/' + id, val);
   }
 
   //page of customers
   pageCustomers(pageNumber: number, pageSize: number) {
-    return this.http.get(this.baseUrl + 'Customers/Page?pageNumber='+pageNumber+'&pageSize='+pageSize);
+    return this.http.get(this.baseUrl + 'Customers/Page?pageNumber=' + pageNumber + '&pageSize=' + pageSize);
   }
 
   //Delete a customer
-  deleteCustomer(id:any){
-    return this.http.delete(this.baseUrl + 'Customers/'+ id);
+  deleteCustomer(id: any) {
+    return this.http.delete(this.baseUrl + 'Customers/' + id);
   }
 
   //get history of customers
-  getHistoryCustomer(id: number){
-    return this.http.get('https://localhost:44305/GetHistory?cutomerId='+ id);
+  getHistoryCustomer(id: number) {
+    return this.http.get('https://localhost:44305/GetHistory?cutomerId=' + id);
   }
 
   // Show list of Appointment through Branch ID
@@ -110,12 +110,12 @@ export class AuthService {
   }
 
   // Create a new Appointment
-  createAppointment(val:any) {
+  createAppointment(val: any) {
     return this.http.post(this.baseUrl + 'Appointment', val);
   }
 
   // Update Appointment
-  UpdateAppointment(id: number, val:any) {
+  UpdateAppointment(id: number, val: any) {
     return this.http.put(this.baseUrl + 'Appointment/' + id, val);
   }
 
@@ -125,7 +125,7 @@ export class AuthService {
   }
 
   //
-  updateAppointmentWithService(id: number, status: any, val:any) {
+  updateAppointmentWithService(id: number, status: any, val: any) {
     return this.http.put(this.baseUrl + 'Appointment/api/UpdateAppointmentWithService/' + id + '/' + status, val);
   }
 
@@ -133,27 +133,29 @@ export class AuthService {
   getAppointment(id: number): Observable<any[]> {
     return this.http.get<any>(this.baseUrl + 'Appointment/' + id);
   }
-  signUp(lastName:string,firstName:string,gender:string,phone:string,email:string,password:string,confirmPassword:string,dateOfBirth:string,hireDate:string,jobTypeID:string,branchID:string,role:string){
-    return this.http.post<{status:object}>(this.baseUrl+'Authentication/register', {
-      lastName : lastName,
-      firstName : firstName,
-      phone : phone,
-      email : email,
-      password : password,
-      confirmPassword : confirmPassword,
-      dateOfBirth : dateOfBirth,
-      hireDate : hireDate,
-      gender : gender,
-      jobTypeID:jobTypeID,
-      branchID:branchID,
-      role : role
+  signUp(lastName: string, firstName: string, gender: string, phone: string, email: string, password: string, confirmPassword: string, dateOfBirth: string, hireDate: string, jobTypeID: string, branchID: string, role: string) {
+    return this.http.post<{ status: object }>(this.baseUrl + 'Authentication/register', {
+      lastName: lastName,
+      firstName: firstName,
+      phone: phone,
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword,
+      dateOfBirth: dateOfBirth,
+      hireDate: hireDate,
+      gender: gender,
+      jobTypeID: jobTypeID,
+      branchID: branchID,
+      role: role
+    })
+  }
   // Update Discount
-  updateDiscount(id: any, discount: any, val:any) {
+  updateDiscount(id: any, discount: any, val: any) {
     return this.http.put(this.baseUrl + 'Appointment/UpdateDiscount?id=' + id + '&perDiscount=' + discount, val);
   }
 
   // Assign Spa Therapist
-  assignSpaTherapist(appointment: any, empID: any, val:any) {
+  assignSpaTherapist(appointment: any, empID: any, val: any) {
     return this.http.put(this.baseUrl + 'Appointment/assigntechnicalstaff?idApp=' + appointment + '&idEmploy=' + empID, val);
   }
 
@@ -174,19 +176,8 @@ export class AuthService {
   }
 
   //
-  signUp(id:string,name:string,email:string,password:string,re_password:string){
-    return this.http.post<{flag:boolean, message:string}>(this.baseUrl+'register', {
-      id: id,
-      name: name,
-      email:email,
-      password: password,
-      confirmPassword: re_password
-    })
-  }
-
-  //
-  login(email:string,password:string){
-    return this.http.post<{flag:boolean,mess:string,token:string}>(this.baseUrl+'Authentication/login', {
+  login(email: string, password: string) {
+    return this.http.post<{ flag: boolean, mess: string, token: string }>(this.baseUrl + 'Authentication/login', {
       email: email,
       password: password
     })
